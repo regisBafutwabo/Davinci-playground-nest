ff5479b851e6fd5fefd9d2ab4ce819722af8cff4
regisBafutwabo <regiswareja@gmail.com>
2023-02-08 11:49:51 +0900

    fix: issues with CORS origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,18 @@ const whiteList = process.env.WHITE_LIST;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    methods: ['GET', 'POST'],
+    origin: [/^(https:\/\/([^\.]*\.)?vercel\.app)$/i, 'http://localhost:3000'],
+    // (origin, callback) => {
+    //   if (!origin || whiteList.indexOf(origin) !== -1) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error('Not allowed by CORS'));
+    //   }
+    // },
+  });
   await app.listen(4000);
 }
 bootstrap();
