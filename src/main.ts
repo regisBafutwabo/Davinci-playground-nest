1b3bb69ef7a35ddf4bae860604362dab05cc76f0
regisBafutwabo <regiswareja@gmail.com>
2023-02-08 11:22:20 +0900

    fix: issues with CORS origin

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+const whiteList = process.env.WHITE_LIST;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: ['https://chat-eosin-two.vercel.app', 'http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    origin: (origin, callback) => {
+      if (!origin || whiteList.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   });
   await app.listen(4000);
 }
