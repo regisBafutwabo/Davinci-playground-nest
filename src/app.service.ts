110a5400f1e4db78b1649b21eeeb96ec419a4f16
regisBafutwabo <regiswareja@gmail.com>
2023-02-08 10:39:51 +0900

    feat: modified the welcom message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! This is chat';
+    return 'Hello World! This is chat API';
   }
 }
