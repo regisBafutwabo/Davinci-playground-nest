921fce54b747a7b53c4ebee4efbae6f1dd3a59bf
regisBafutwabo <regiswareja@gmail.com>
2023-02-08 10:47:25 +0900

    feat: added new CORS origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://chat-eosin-two.vercel.app'],
   });
   await app.listen(4000);
 }
