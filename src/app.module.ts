16e523380bb3f15e9a390317acf03158e083717f
regisBafutwabo <regiswareja@gmail.com>
2023-03-02 17:05:32 +0900

    fix: issue with connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,27 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
-import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [databaseConfig],
+      load: [],
     }),
     ChatModule,
-    // AuthModule,
-    // UserModule,
-    // PassportModule,
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'postgres',
-    //     url: configService.get('database.url'),
-    //     ssl: true,
-    //     entities: [UserEntity],
-    //   }),
-    //   inject: [ConfigService],
-    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
